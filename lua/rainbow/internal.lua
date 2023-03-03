c9a32549ea767d5dfc4d5c7fa13d2624182f3934
林千里 <lincheney@gmail.com>
2023-03-03 17:53:32 +1100

    switch to using nvim_set_decoration_provider instead
    instead of updating extmarks on every tree change, calculate and cache
    the nesting results on tree change
    then set ephemeral extmarks only on the lines that
    nvim_set_decoration_provider tells us we need to

--- lua/rainbow/internal.lua
+++ lua/rainbow/internal.lua
@@ -1,4 +1,5 @@
 --[[
+   Copyright 2023 Cheney Lin
    Copyright 2020-2022 Chinmay Dalal
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +15,8 @@
    limitations under the License.
 --]]
 
+local M = {}
+
 local queries = require('nvim-treesitter.query')
 local parsers = require('nvim-treesitter.parsers')
 local configs = require('nvim-treesitter.configs')
@@ -25,37 +28,50 @@ local extended_languages = { 'latex', 'html', 'verilog', 'jsx' }
 local colors = configs.get_module('rainbow').colors
 local termcolors = configs.get_module('rainbow').termcolors
 
---- Maps a buffer ID to the buffer's parser; retaining a reference prevents the
---- parser from getting garbage-collected.
-local buffer_parsers = {}
+local state_table = {}
 
---- Find the nesting level of a node.
---- @param node table # Node to find the level of
---- @param len number # Number of colours
---- @param levels table # Levels for the language
-local function color_no(node, len, levels)
-  local counter = 0
-  local current = node
-  local found = false
-  while current:parent() ~= nil do
-    if levels then
-      if levels[current:type()] then
-        counter = counter + 1
-        found = true
-      end
-    else
-      counter = counter + 1
-      found = true
-    end
-    current = current:parent()
-  end
-  if not found then
+local function tuple_cmp(x, y)
+  if     x[1] < y[1] then
+    return -1
+  elseif x[1] > y[1] then
     return 1
-  elseif counter % len == 0 then
-    return len
+  elseif x[2] < y[2] then
+    return -1
+  elseif x[2] > y[2] then
+    return 1
   else
-    return (counter % len)
+    return 0
   end
+end
+
+local function range_overlap(x, y)
+  return tuple_cmp(x, {y[3], y[4]}) < 0 and tuple_cmp(y, {x[3], x[4]}) < 0
+end
+
+local function binsearch_items(items, target, start)
+  local start = start or 1
+  local finish = #items
+  local mid
+
+  while start <= finish do
+    mid = math.floor((start + finish) / 2)
+    -- we are just comparing start ; is that good enough?
+    local cmp = tuple_cmp(target, items[mid].start)
+    if cmp < 0 then
+      finish = mid - 1
+    elseif cmp == 0 or tuple_cmp(target, items[mid].finish) < 0 then
+      return mid, true
+    else
+      start = mid + 1
+    end
+  end
+  return start
+end
+
+local function get_items_in_range(items, start, finish)
+  local start = binsearch_items(items, start)
+  local finish = binsearch_items(items, finish, start)
+  return start, finish
 end
 
 --- Update highlights for a range. Called every time text is changed.
@@ -63,61 +79,91 @@ end
 --- @param changes table # Range of text changes
 --- @param tree table # Syntax tree
 --- @param lang string # Language
+--- Update highlights for a range. Called every time text is changed.
+--- @param bufnr number # Buffer number
+--- @param changes table # Range of text changes
+--- @param tree table # Syntax tree
+--- @param lang string # Language
 local function update_range(bufnr, changes, tree, lang)
+  if #changes == 0 then
+    return
+  end
+
   if vim.fn.pumvisible() ~= 0 or not lang then
     return
   end
 
-  for _, change in ipairs(changes) do
-    local root_node = tree:root()
-    local query = queries.get_query(lang, 'parens')
-    local levels = require('rainbow.levels')[lang]
-    local capture_extmarks = api.nvim_buf_get_extmarks(bufnr, nsid, { change[1], 0 }, { change[3] + 1, 0 }, {})
-    for _, extmark in ipairs(capture_extmarks) do
-      api.nvim_buf_del_extmark(bufnr, nsid, extmark[1])
-    end
-    if query ~= nil then
-      for _, node, _ in query:iter_captures(root_node, bufnr, change[1], change[3] + 1) do
-        -- set colour for this nesting level
-        if not node:has_error() then
-          local color_no_ = color_no(node, #colors, levels)
-          local startRow, startCol, endRow, endCol = node:range() -- range of the capture, zero-indexed
-          if vim.fn.has('nvim-0.7') == 1 then
-            vim.highlight.range(bufnr, nsid, ('rainbowcol' .. color_no_), { ---@diagnostic disable-line
-              startRow,
-              startCol,
-            }, {
-              endRow,
-              endCol - 1,
-            }, {
-              regtype = 'b',
-              inclusive = true,
-              priority = 210,
-            })
-          else
-            ---@diagnostic disable
-            vim.highlight.range(
-              bufnr,
-              nsid,
-              ('rainbowcol' .. color_no_),
-              { startRow, startCol },
-              { endRow, endCol - 1 },
-              'blockwise',
-              true,
-              210
-            )
-            ---@diagnostic enable
+  local root = tree:root()
+
+  -- load the query
+  local query = queries.get_query(lang, "parens")
+  local items = state_table[bufnr].items[lang] or {}
+  -- invalidate everything for now
+  -- figure out how to do damage later
+  items = {}
+
+  local seen = {}
+  local stack = {}
+  local scopes = {}
+
+  for id, node, metadata in query:iter_captures(root, bufnr, 0, -1) do
+    if node:has_error() then
+    elseif seen[node:id()] then
+      -- skip nodes we have already processed
+      -- this can happen if a node is captured multiple times
+    else
+      seen[node:id()] = true
+      local name = query.captures[id]
+      local type = 'parens'
+
+      local item = {type=type, matched=false, start={node:start()}, finish={node:end_()}}
+
+      if name == 'left' then
+        -- add to stack
+        item.open = true
+        table.insert(stack, item)
+        table.insert(items, item)
+
+      elseif name == 'right' then
+        -- find a matching opening bracket
+        item.open = false
+        local index
+        for i = 0, #stack-1 do
+          local x = stack[#stack-i]
+          if x.type == type then
+            x.matched = true
+            item.matched = true
+            stack = vim.list_slice(stack, 1, #stack-i-1)
+            break
           end
         end
+        table.insert(items, item)
+
+      end
+
+    end
+  end
+
+  -- set the level of each bracket, starting from 0
+  local level = 0
+  for _, item in ipairs(items) do
+    if item.matched then
+      if item.open then
+        level = level + 1
+        item.level = level
+      else
+        item.level = level
+        level = level - 1
       end
     end
   end
+  state_table[bufnr].items[lang] = items
 end
 
 --- Update highlights for every tree in given buffer.
 --- @param bufnr number # Buffer number
 local function full_update(bufnr)
-  local parser = buffer_parsers[bufnr]
+  local parser = parsers.get_parser(bufnr)
   parser:invalidate(true)
   parser:parse()
   parser:for_each_tree(function(tree, sub_parser)
@@ -154,10 +200,6 @@ local function register_predicates(config)
   end
 end
 
-local state_table = {}
-
-local M = {}
-
 --- Define highlight groups. This had to be a function to allow an autocmd doing this at colorscheme change.
 function M.defhl()
   for i = 1, math.max(#colors, #termcolors) do
@@ -185,27 +227,27 @@ function M.attach(bufnr, lang)
   if max_file_lines ~= nil and vim.api.nvim_buf_line_count(bufnr) > max_file_lines then
     return
   end
-  register_predicates(config)
+
+  -- register_predicates(config)
+  state_table[bufnr] = {
+    changes={},
+    items={},
+  }
   local parser = parsers.get_parser(bufnr, lang)
-  ---@diagnostic disable-next-line
   parser:register_cbs({
     on_changedtree = function(changes, tree)
       if state_table[bufnr] then
-        update_range(bufnr, changes, tree, lang)
-      else
-        return
+        vim.list_extend(state_table[bufnr].changes, changes)
       end
     end,
   })
-  buffer_parsers[bufnr] = parser
-  state_table[bufnr] = true
   full_update(bufnr)
 end
 
 --- Detach module from buffer. Called when `:TSBufDisable rainbow`.
 --- @param bufnr number # Buffer number
 function M.detach(bufnr)
-  state_table[bufnr] = false
+  state_table[bufnr] = nil
   ---@diagnostic disable-next-line
   if vim.treesitter.highlighter.hl_map then
     ---@diagnostic disable-next-line
@@ -213,24 +255,51 @@ function M.detach(bufnr)
   else
     vim.api.nvim_set_hl(0, '@punctuation.bracket', { link = 'TSPunctBracket' })
   end
-  vim.api.nvim_buf_clear_namespace(bufnr, nsid, 0, -1)
-  buffer_parsers[bufnr] = nil
 end
 
-if vim.fn.has('nvim-0.7') == 1 then
-  api.nvim_create_augroup('RainbowParser', {})
-  api.nvim_create_autocmd('FileType', {
-    group = 'RainbowParser',
-    pattern = '*',
-    callback = function()
-      local bufnr = api.nvim_get_current_buf()
-      if state_table[bufnr] then
-        local lang = parsers.get_buf_lang(bufnr)
-        local parser = parsers.get_parser(bufnr, lang)
-        buffer_parsers[bufnr] = parser
+local function on_line(_, win, bufnr, row)
+  if not state_table[bufnr] then
+    return
+  end
+
+  if #state_table[bufnr].changes > 0 then
+    state_table[bufnr].parser:for_each_tree(function(tree, sub_parser)
+      update_range(bufnr, state_table[bufnr].changes, tree, sub_parser:lang())
+    end)
+    state_table[bufnr].changes = nil
+  end
+
+  local size = #configs.get_module('rainbow').colors
+  for lang, items in pairs(state_table[bufnr].items) do
+    local start, finish = get_items_in_range(items, {row, 0}, {row+1, 0})
+    for i = start, finish-1 do
+      local item = items[i]
+      if item.level then
+
+        if not item.hl then
+          item.hl = 'Error'
+          if item.matched or item.middle then
+            item.hl = 'rainbowcol'..tostring((item.level-1) % size + 1)
+          end
+        end
+
+        vim.api.nvim_buf_set_extmark(bufnr, nsid, item.start[1], item.start[2], {
+          end_line = item.finish[1],
+          end_col = item.finish[2],
+          hl_group = item.hl,
+          ephemeral = true,
+          priority = 1000,
+        })
       end
-    end,
-  })
+    end
+  end
 end
+
+vim.api.nvim_set_decoration_provider(nsid, {
+  on_win = function(_, winnr, bufnr, topline, botline_guess)
+    return state_table[bufnr] and true
+  end,
+  on_line = on_line,
+})
 
 return M
