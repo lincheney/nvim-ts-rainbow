b0c5a682b5fac80e4ffbc84c53a86040a6968254
林千里 <lincheney@gmail.com>
2023-03-03 21:38:17 +1100

    add some lua highlighting

---
+++ queries/lua/parens.scm
@@ -0,0 +1,10 @@
+; inherits: square,round,curly
+"function" @left.block
+"while" @left.block
+"for" @left.block
+"do" @middle.block
+"if" @left.block
+"then" @middle.block
+"elseif" @middle.block
+"else" @middle.block
+"end" @right.block
