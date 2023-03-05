dab77fafcca937b2a065072eb626bc7f286f1837
林千里 <lincheney@gmail.com>
2023-03-05 14:41:34 +1100

    fix yaml sequence capture names

--- queries/yaml/parens.scm
+++ queries/yaml/parens.scm
@@ -3,5 +3,5 @@
 (block_mapping) @scope.mapping
 (block_mapping_pair . key: _ @middle.mapping)
 
-(block_sequence) @scope.mapping
-(block_sequence_item . "-" @middle.mapping)
+(block_sequence) @scope.sequence
+(block_sequence_item . "-" @middle.sequence)
