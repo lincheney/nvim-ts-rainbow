fcde99d830de626ccb5d5532a9e4bb18fc6adf27
林千里 <lincheney@gmail.com>
2023-03-05 13:56:41 +1100

    queries for yaml

---
+++ queries/yaml/parens.scm
@@ -0,0 +1,7 @@
+; inherits curly,square,comma
+
+(block_mapping) @scope.mapping
+(block_mapping_pair . key: _ @middle.mapping)
+
+(block_sequence) @scope.mapping
+(block_sequence_item . "-" @middle.mapping)
