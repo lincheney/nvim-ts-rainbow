c77b6e7bd432b4a1607c95efd7460e69265bb15f
林千里 <lincheney@gmail.com>
2023-03-17 22:26:56 +1100

    query for html

--- queries/html/parens.scm
+++ queries/html/parens.scm
@@ -1,4 +1,24 @@
-; inherits: angle
+(element
+    . (start_tag
+        "<" @middle.element
+        (tag_name) @middle.element
+        (attribute (attribute_name) @middle.element)*
+        ">" @middle.element
+    ) @skip.start_tag
 
-"</" @left.angle
-"/>" @right.angle
+    (end_tag
+        "</" @middle.element
+        (tag_name) @middle.element
+        ">" @middle.element
+    ) .
+) @scope.element
+
+(self_closing_tag
+    "<" @middle.element
+    (tag_name) @middle.element
+    (attribute (attribute_name) @middle.element)*
+    "/>" @middle.element
+)
+
+(element . (start_tag) @left.erroneous_start_tag )
+(erroneous_end_tag) @right.erroneous_end_tag
