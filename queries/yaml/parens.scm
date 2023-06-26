bb7e3a1f7f33181089b3cb01e35e7ea9244ea5df
林千里 <lincheney@gmail.com>
2023-06-26 22:35:29 +1000

    allow jumping over yaml keys

--- queries/yaml/parens.scm
+++ queries/yaml/parens.scm
@@ -1,7 +1,7 @@
 ; inherits _curly,_square,_comma
 
 (block_mapping) @scope.mapping
-(block_mapping_pair . key: _ @middle.mapping)
+(block_mapping_pair . key: _ @middle.mapping (#set! "jump" "true"))
 
 (block_sequence) @scope.sequence
 (block_sequence_item . "-" @middle.sequence)
