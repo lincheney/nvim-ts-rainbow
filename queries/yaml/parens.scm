49d8a9bb856a6d027e754d032b8495cd0ad328c3
林千里 <lincheney@gmail.com>
2023-06-29 22:18:03 +1000

    jump over yaml sequences too

--- queries/yaml/parens.scm
+++ queries/yaml/parens.scm
@@ -4,4 +4,4 @@
 (block_mapping_pair . key: _ @middle.mapping (#set! "jump" "true"))
 
 (block_sequence) @scope.sequence
-(block_sequence_item . "-" @middle.sequence)
+(block_sequence_item . "-" @middle.sequence (#set! "jump" "true"))
