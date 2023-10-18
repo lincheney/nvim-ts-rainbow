d4c985fc601cd69e77fe20c6bdd470d8779c95df
林千里 <lincheney@gmail.com>
2023-10-18 19:07:51 +1100

    better html queries

--- queries/html/parens.scm
+++ queries/html/parens.scm
@@ -1,24 +1,16 @@
 (element
-    . (start_tag
-        "<" @middle.element
-        (tag_name) @middle.element
-        (attribute (attribute_name) @middle.element)*
-        ">" @middle.element
-    ) @skip.start_tag
-
-    (end_tag
-        "</" @middle.element
-        (tag_name) @middle.element
-        ">" @middle.element
-    ) .
+    . (start_tag "<" @middle.angle ">" @middle.angle) @skip.start_tag
+    (end_tag "</" @middle.angle ">" @middle.angle) .
 ) @scope.element
 
-(self_closing_tag
-    "<" @middle.element
-    (tag_name) @middle.element
-    (attribute (attribute_name) @middle.element)*
-    "/>" @middle.element
+(self_closing_tag "<" @middle.angle "/>" @middle.angle) @scope.self_closing_tag
+
+(_
+    . (start_tag)
+    (_ . (start_tag "<" @middle.angle ">" @middle.angle) @scope.self_closing_tag)
+    (end_tag) .
 )
 
-(element . (start_tag) @left.erroneous_start_tag )
+(attribute (attribute_name) @middle.attr)*
+(_ (tag_name) @middle.tag_name (#set! "jump" "true"))
 (erroneous_end_tag) @right.erroneous_end_tag
