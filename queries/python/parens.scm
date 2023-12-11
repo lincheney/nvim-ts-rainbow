d506b49de61140ea4f3bd91ddc757d31818fba47
林千里 <lincheney@gmail.com>
2023-12-11 17:00:14 +1100

    indent information for python and yaml

--- queries/python/parens.scm
+++ queries/python/parens.scm
@@ -2,18 +2,20 @@
 
 (interpolation) @scope.interpolation
 
-(for_statement) @scope.for
-(for_statement "for" @middle.for (#set! "jump" "true") "in" @middle.for ":" @middle.for)
-(for_statement (else_clause "else" @middle.for (#set! "jump" "true") ":" @middle.for))
+; do not indent next line if inline block
+(_ (block _) @middle._block (#not-lua-match? @middle._block "^%s*$") (#set! indent_next -999) (#set! no_highlight true))
+; do not indent in string
+(string (#set! indent_next 0)) @scope.string
 
-(while_statement) @scope.while
-(while_statement "while" @middle.while (#set! "jump" "true") ":" @middle.while)
-(while_statement (else_clause "else" @middle.while (#set! "jump" "true") ":" @middle.while))
+; indent next line if no block
+(for_statement "for" @middle.for "in" @middle.for ":" @middle.for) @scope.for
+(for_statement (else_clause "else" @middle.for ":" @middle.for))
 
-(if_statement) @scope.if
-(if_statement "if" @middle.if (#set! "jump" "true") ":" @middle.if)
-(if_statement (else_clause "else" @middle.if (#set! "jump" "true") ":" @middle.if))
-(elif_clause "elif" @middle.if ":" @middle.if (#set! "jump" "true"))
+(while_statement "while" @middle.while ":" @middle.while) @scope.while
+(while_statement (else_clause "else" @middle.while ":" @middle.while))
+
+(if_statement "if" @middle.if ":" @middle.if) @scope.if
+(if_statement (_ ["else" "elif"] @middle.if ":" @middle.if))
 
 (with_statement "with" @middle.with ":" @middle.with) @scope.with
 
@@ -21,14 +23,23 @@
 
 (class_definition "class" @middle.class ":" @middle.class) @scope.class
 
-(try_statement) @scope.try
-(try_statement "try" @middle.try (#set! "jump" "true") ":" @middle.try)
-(ERROR . "try" @middle.try (#set! "jump" "true") ":" @middle.try) @scope.try
-(try_statement (except_clause "except" @middle.try (#set! "jump" "true") ":" @middle.try))
+(try_statement "try" @middle.try ":" @middle.try) @scope.try
+(ERROR . "try" @middle.try.text ":" @middle.try (#eq? @middle.try.text "try") (#set! jump true) ) @scope.try
 ; (except_group_clause "except*" @middle.try ":" @middle.try)?
-(try_statement (else_clause "else" @middle.try (#set! "jump" "true") ":" @middle.try))
-(try_statement (finally_clause "finally" @middle.try (#set! "jump" "true") ":" @middle.try))
+(try_statement (_  ["except" "else" "finally"]  @middle.try ":" @middle.try))
 
-(match_statement) @scope.match
-(match_statement "match" @middle.match (#set! "jump" "true") ":" @middle.match)
-(match_statement (block (case_clause "case" @middle.match (#set! "jump" "true") ":" @middle.match)))
+(match_statement "match" @middle.match ":" @middle.match) @scope.match
+(match_statement (block (case_clause "case" @middle.match ":" @middle.match (#set! align_with "^match$") (#set! align_offset 1) )))
+
+(ERROR . (_ "except")  @left._except  (#eq? @left._except  "except")  (#set! align_with "^try$"))
+(ERROR . (_ "else")    @left._else    (#eq? @left._else    "else")    (#set! align_with "\\v^(for|while|if|try)$"))
+(ERROR . (_ "elif")    @left._elif    (#eq? @left._elif    "elif")    (#set! align_with "^if$"))
+(ERROR . (_ "finally") @left._finally (#eq? @left._finally "finally") (#set! align_with "^try$"))
+(ERROR . (_ "case")    @left._case    (#eq? @left._case    "case")    (#set! align_with "^match$") (#set! align_offset 1) )
+
+; can jump to the following nodes
+(["if" "for" "while" "try" "else" "elif" "except" "finally" "match" "case"] @_nomatch (#set! jump true))
+; indent after these nodes
+(["else" "elif" "except" "finally" "case"] @_nomatch (#set! indent_next 1))
+; dedent after these nodes
+(_ ["pass" "raise" "return"] @middle._noindent (#set! no_highlight true) (#set! indent_next -1) )
--- queries/yaml/parens.scm
+++ queries/yaml/parens.scm
@@ -1,7 +1,13 @@
 ; inherits _curly,_square,_comma
 
+(block_scalar) @scope.scalar
+
 (block_mapping) @scope.mapping
-(block_mapping_pair . key: _ @middle.mapping (#set! "jump" "true"))
+(block_mapping_pair . key: _ @middle.mapping (#set! jump true) (#set! indent_next 1) (#set! align_with mapping) (#set! align_start true) )
+; do not indent next line if inline value
+(block_mapping_pair value: (flow_node) @middle._value (#set! indent_next 0) (#set! no_highlight true))
 
 (block_sequence) @scope.sequence
-(block_sequence_item . "-" @middle.sequence (#set! "jump" "true"))
+(block_sequence_item . "-" @middle.sequence (#set! jump true) (#set! indent_next 1))
+; do not indent next line if inline value
+(block_sequence_item (flow_node) @middle._value . (#set! indent_next 0) (#set! no_highlight true) (#set! align_with sequence) )
