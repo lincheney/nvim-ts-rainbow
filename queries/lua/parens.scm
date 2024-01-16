d46059140b805ae87a97adbe5635dbb41d9e8308
林千里 <lincheney@gmail.com>
2024-01-16 12:08:03 +1100

    lua: fix some handling of elseif

--- queries/lua/parens.scm
+++ queries/lua/parens.scm
@@ -18,10 +18,11 @@
 (for_statement "do" @middle.block)
 (for_statement "end" @right.block)
 
-("if" @left.if (#set! "right" " then"))
+("if" @left.if (#set! "right" " then\n"))
 ("if" @left.if (#set! "right" "\nend") "then" @middle.if (#eq? @middle.if "then"))
 (ERROR ("if" @left.if (#set! "right" "\nend")) ("then" @middle.if (#eq? @middle.if "then") ))
-("elseif" @middle.if (#set! "jump" "true") (#set! align_with if))
+("elseif" @middle.if (#set! "jump" "true") (#set! align_with if) ("then" @middle.if (#eq? @middle.if "then") ))
+("elseif" @left.wrong (#set! align_with if) (#set! "right" " then\n"))
 ("else" @middle.if (#set! "jump" "true") (#set! align_with if))
 (if_statement "end" @right.if)
 
@@ -29,5 +30,5 @@
 "until" @right.repeat-until
 
 ; then anywhere else is wrong
-"then" @right.wrong
-"end" @right.wrong
+"then" @left.wrong
+"end" @left.wrong
