916de09d80ed2cc25cf5e9a08e946fdc8dd8cfd0
林千里 <lincheney@gmail.com>
2024-05-15 19:25:16 +1000

    use indent_next_at_most with yaml

--- queries/yaml/parens.scm
+++ queries/yaml/parens.scm
@@ -3,11 +3,11 @@
 (block_scalar) @scope.scalar
 
 (block_mapping) @scope.mapping
-(block_mapping_pair . key: _ @middle.mapping (#set! jump true) (#set! indent_next 1) (#set! align_with mapping) (#set! align_start true) )
+(block_mapping_pair . key: _ @middle.mapping (#set! jump true) (#set! indent_next 1)  (#set! align_start true) )
 ; do not indent next line if inline value
-(block_mapping_pair value: (flow_node) @middle._value (#set! indent_next 0) (#set! no_highlight true))
+(block_mapping_pair value: (flow_node) @middle._value (#set! indent_next_at_most 0) (#set! no_highlight true))
 
 (block_sequence) @scope.sequence
-(block_sequence_item . "-" @middle.sequence (#set! jump true) (#set! indent_next 1))
+(block_sequence_item . "-" @middle.sequence (#set! jump true) (#set! indent_next_at_most 1))
 ; do not indent next line if inline value
-(block_sequence_item (flow_node) @middle._value . (#set! indent_next 0) (#set! no_highlight true) (#set! align_with sequence) )
+(block_sequence_item (flow_node) @middle._value . (#set! indent_next_at_most 0) (#set! no_highlight true) (#set! align_with sequence) )
