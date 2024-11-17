0cd0fdb2914dd43cf4d569fb923a27bf060ac1d6
林千里 <lincheney@gmail.com>
2024-11-18 00:28:27 +1100

    jsx: handle when tag is a member_expression

--- queries/jsx/parens.scm
+++ queries/jsx/parens.scm
@@ -1,27 +1,27 @@
 (
-   (jsx_opening_element name: (identifier) @left.erroneous_start_tag)
-   (jsx_closing_element name: (identifier) @right.erroneous_end_tag)
+   (jsx_opening_element name: [(identifier) (member_expression)] @left.erroneous_start_tag)
+   (jsx_closing_element name: [(identifier) (member_expression)] @right.erroneous_end_tag)
    (#not-eq? @left.erroneous_start_tag @right.erroneous_end_tag)
 )
 
 (_
   . (jsx_opening_element
     "<" @middle.angle
-    name: (identifier) @middle.tag_name
+    name: [(identifier) (member_expression)] @middle.tag_name
     ">" @middle.angle
   ) @left.tag (#set! @left.tag "no_highlight" "true")
-  (jsx_closing_element name: (identifier) @skip.close_tag) @right.tag
+  (jsx_closing_element name: [(identifier) (member_expression)] @skip.close_tag) @right.tag
   (#eq? @skip.close_tag @middle.tag_name)
 )
 
 (ERROR
   . (jsx_opening_element
-    name: (identifier) @skip.tag_name
+    name: [(identifier) (member_expression)] @skip.tag_name
 ) @left.erroneous_start_tag )
 
 (jsx_self_closing_element
   "<" @middle.angle
-  name: (identifier) @middle.tag_name
+  name: [(identifier) (member_expression)] @middle.tag_name
   "/>" @middle.angle
 ) @scope.self_closing_tag
 
