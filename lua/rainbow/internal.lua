16c05c6fb7beb03ee5e1f20d011de8276899a4fb
林千里 <lincheney@gmail.com>
2025-07-20 02:26:57 +1000

    dont need line shift if 0

--- lua/rainbow/internal.lua
+++ lua/rainbow/internal.lua
@@ -335,8 +335,8 @@ local function process_on_bytes(state, args)
     local finish_line_shift = line_shift ~= 0 and tuple_cmp(item.finish, start) > 0
     local edge_finish_line_shift = line_shift ~= 0 and tuple_cmp(item.finish, start) >= 0
     -- on the last line, shift it horizontally
-    local start_col_shift = start_line_shift and col_shift ~= 0 and item.start[1] == finish[1] and item.start[2] >= finish[2]
-    local finish_col_shift = finish_line_shift and col_shift ~= 0 and item.finish[1] == finish[1] and item.finish[2] >= finish[2]
+    local start_col_shift = (line_shift == 0 or start_line_shift) and col_shift ~= 0 and item.start[1] == finish[1] and item.start[2] >= finish[2]
+    local finish_col_shift = (line_shift == 0 or finish_line_shift) and col_shift ~= 0 and item.finish[1] == finish[1] and item.finish[2] >= finish[2]
 
     if start_col_shift then
       item.start[2] = item.start[2] + col_shift
